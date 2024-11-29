ffbd73f2a283685458e2a188999c70cd877d7700
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-29 16:57:57 +0100

    add descrizione al click sul bottone

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import languages from './assets/languages'
 import Header from './components/Header/Header'
 import Button from './components/Button/Button'
 
 
 function App() {
-  const [selectedLanguage, setSelectedLanguage] = useState(0)
-
-
 
   return (
     <>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,30 @@
 import languages from "../../assets/languages";
 import style from './Button.module.css'
-
+import { useState } from 'react'
 
 export default function Button() {
+  const [selectedLanguage, setSelectedLanguage] = useState(null)
+
+  const buttonSelected = (description) => {
+    setSelectedLanguage(description)
+  }
   return (
     <div>
       {
-        languages.map((name) =>
-          <button className={style.button} key={name.id}>
-            {name.title}
+        languages.map((language) =>
+          <button onClick={() => buttonSelected(language.description)} className={style.button} key={language.id}>
+            {language.title}
           </button>
         )
       }
+      <div className={style.container}>
+        {selectedLanguage ? (
+          <p>{selectedLanguage}</p>
+        ) : (
+          <p>Seleziona un linguaggio per vedere la descrizione.</p>
+        )}
+      </div>
     </div>
+
   )
 }
